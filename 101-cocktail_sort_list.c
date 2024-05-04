032dba83f7cd959bbfc648bc32d7f8652614f1b4
khawla_ben <refeser52@gmail.com>
2024-05-04 19:29:50 +0000

    updates

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -9,55 +9,53 @@
 void cocktail_sort_list(listint_t **list)
 {
     int swapped;
-    listint_t *current;
+    listint_t *left, *right, *current;
 
     if (list == NULL || *list == NULL)
         return;
-
     do {
         swapped = 0;
-        for (current = *list; current->next != NULL; current = current->next)
+        for (left = *list; left->next != NULL; left = left->next)
         {
-            if (current->n > current->next->n)
+            if (left->n > left->next->n)
             {
-                if (current->prev != NULL)
-                    current->prev->next = current->next;
-                else
-                    *list = current->next;
-                current->next->prev = current->prev;
-                current->prev = current->next;
-                current->next = current->next->next;
-                current->prev->next = current;
-                if (current->next != NULL)
-                    current->next->prev = current;
                 swapped = 1;
+                if (left->prev != NULL)
+                    left->prev->next = left->next;
+                else
+                    *list = left->next;
+                left->next->prev = left->prev;
+                left->prev = left->next;
+                left->next = left->next->next;
+                left->prev->next = left;
+                if (left->next != NULL)
+                    left->next->prev = left;
                 print_list(*list);
                 printf("\n");
             }
         }
-
         if (!swapped)
             break;
-
         swapped = 0;
-        for (; current->prev != NULL; current = current->prev)
+        for (right = left->prev; right != NULL; right = right->prev)
         {
-            if (current->n < current->prev->n)
+            if (right->prev != NULL && right->n < right->prev->n)
             {
-                if (current->next != NULL)
-                    current->next->prev = current->prev;
-                current->prev->next = current->next;
-                current->next = current->prev;
-                current->prev = current->prev->prev;
-                current->next->prev = current;
-                if (current->prev != NULL)
-                    current->prev->next = current;
-                else
-                    *list = current;
                 swapped = 1;
+                if (right->next != NULL)
+                    right->next->prev = right->prev;
+                right->prev->next = right->next;
+                right->next = right->prev;
+                right->prev = right->prev->prev;
+                right->next->prev = right;
+                if (right->prev != NULL)
+                    right->prev->next = right;
+                else
+                    *list = right;
                 print_list(*list);
                 printf("\n");
             }
         }
-    } while (swapped);
+    }
+    while (swapped);
 }
